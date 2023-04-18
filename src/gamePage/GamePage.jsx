9e6e183bbf820a365b0354b0808f32d7cf792763
Oluniyisrael <israeloluniyi2004@gmail.com>
2023-04-18 13:30:32 +0100

    this.start.project

--- src/gamePage/GamePage.jsx
+++ src/gamePage/GamePage.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
-import './Gamepage.css'
+import './GamePage.css'
 
 function GamePage() {
+  // function changeClick{
+  //   if()
+  // }
   return (
     <div className='table'>
         <div className='tableCont'>
             <table className='XO'>
               <tr className='tableRow'>
+                <td className='tableData' onClick={(e)=>{console.log(e.target)}}></td>
                 <td className='tableData'></td>
-                <td></td>
-                <td></td>
+                <td className='tableData'></td>
               </tr>
               <tr className='tableRow'>
                 <td className='tableData'></td>
-                <td></td>
-                <td></td>
+                <td className='tableData'></td>
+                <td className='tableData'></td>
               </tr>
               <tr className='tableRow'>
                 <td className='tableData'></td>
-                <td></td>
-                <td></td>
+                <td className='tableData'></td>
+                <td className='tableData'></td>
               </tr>
             </table>
         </div>
