19b3b8ba7d2cde808f5c40d2562dcef7c532b565
Oluniyisrael <israeloluniyi2004@gmail.com>
2023-04-21 12:39:28 +0100

    Bug fixed

--- src/gamePage/GamePage.jsx
+++ src/gamePage/GamePage.jsx
@@ -11,30 +11,30 @@ function GamePage() {
 
   const [count,setCount] = useState(0)
   const [choice,setChoice]= useState(0)
-  // function declearWin() {
-  //   var squares = document.getElementsByClassName('tableData');
-  //   if(squares[0].innerHTML === squares[1].innerHTML && squares[1].innerHTML === squares[2].innerHTML && squares[2].innerHTML !== '' 
-  //       ||
-  //       squares[0].innerHTML === squares[3].innerHTML && squares[3].innerHTML === squares[6].innerHTML && squares[6].innerHTML !== ''
-  //       ||
-  //       squares[0].innerHTML === squares[4].innerHTML && squares[4].innerHTML === squares[8].innerHTML && squares[8].innerHTML !== ''
-  //       ||
-  //       squares[1].innerHTML === squares[4].innerHTML && squares[4].innerHTML === squares[7].innerHTML && squares[7].innerHTML !== ''
-  //       ||
-  //       squares[2].innerHTML === squares[5].innerHTML && squares[5].innerHTML === squares[8].innerHTML && squares[8].innerHTML !== ''
-  //       ||
-  //       squares[3].innerHTML === squares[4].innerHTML && squares[4].innerHTML === squares[5].innerHTML && squares[5].innerHTML !== ''
-  //       ||
-  //       squares[6].innerHTML === squares[7].innerHTML && squares[7].innerHTML === squares[8].innerHTML && squares[6].innerHTML !== ''
-  //       // ||
+  function declearWin() {
+    var squares = document.getElementsByClassName('tableData');
+    if(squares[0].innerText === squares[1].innerText && squares[1].innerText === squares[2].innerText && squares[2].innerText !== '' 
+        ||
+        squares[0].innerText === squares[3].innerText && squares[3].innerText === squares[6].innerText && squares[6].innerText !== ''
+        ||
+        squares[0].innerText === squares[4].innerText && squares[4].innerText === squares[8].innerText && squares[8].innerText !== ''
+        ||
+        squares[1].innerText === squares[4].innerText && squares[4].innerText === squares[7].innerText && squares[7].innerText !== ''
+        ||
+        squares[2].innerText === squares[5].innerText && squares[5].innerText === squares[8].innerText && squares[8].innerText !== ''
+        ||
+        squares[3].innerText === squares[4].innerText && squares[4].innerText === squares[5].innerText && squares[5].innerText !== ''
+        ||
+        squares[6].innerText === squares[7].innerText && squares[7].innerText === squares[8].innerText && squares[6].innerText !== ''
+        // ||
 
-  //       ){
-  //     console.log(' we have a winner')
-  //   }
-  //   else{console.log('No one won')}
-  // } 
+        ){
+      console.log(' we have a winner')
+    }
+    else{console.log('No one won')}
+  } 
   if (count >= 5 ){
-    // declearWin()
+    declearWin()
   }      
   function add(e){
         setCount(count + 1);
@@ -49,6 +49,13 @@ function GamePage() {
             setChoice(choice - 2)
           }
         }
+        // function checkWin(){
+        //   var squares = document.getElementsByClassName('tableData');
+        //   if(squares[0].innerText === squares[1].innerText && squares[1].innerText === squares[2].innerText && squares[2].innerText !== ''){
+        //       console.log('yes')
+        //   }else{console.log('no')
+        //        }
+        // }
         e.target.className='changeNode'
         e.target.parentNode.innerText = writings[choice][count]
 
