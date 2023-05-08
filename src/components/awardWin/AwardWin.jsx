0dd2933b1ef5069485839ae90a13dbe0e525ae13
Oluniyisrael <israeloluniyi2004@gmail.com>
2023-05-08 08:39:08 +0100

    Still recomponenting

--- src/components/awardWin/AwardWin.jsx
+++ src/components/awardWin/AwardWin.jsx
@@ -1,8 +1,6 @@
 function AwardWin(Number,props){
     const refresh = ()=>{props.refresh()}
     const squares = document.getElementsByClassName('tableData');
-
-    document.getElementById('line').style.display = 'block';
           if (squares[Number].innerText === 'X') {
             props.addX()          }
           else{props.addO()}
--- src/components/declearWin/DeclearWin.jsx
+++ src/components/declearWin/DeclearWin.jsx
@@ -5,69 +5,45 @@ function DeclearWin(props) {
         if(squares[0].innerText === squares[1].innerText && squares[1].innerText === squares[2].innerText && squares[2].innerText !== '' 
             ){
           console.log(' Case1 win');
+          document.getElementById('line').style.display = 'block'
           AwardWin(2,props)
-          // so what i am to do now is to create a score board, find which winning cases bring out the possible variables and set scores coressponing to the variable outcome
-          //done 
-          //Recomponet-sizing function
         }
         else if(squares[0].innerText === squares[3].innerText && squares[3].innerText === squares[6].innerText && squares[6].innerText !== ''){
           console.log(' Case2 win')
           document.getElementById('line2').style.display = 'block'
-          if (squares[6].innerText === 'X') {
-            props.addX()   }
-          else{props.addO()}
-          // refresh()
+          AwardWin(6,props)
         }
         else if(squares[0].innerText === squares[4].innerText && squares[4].innerText === squares[8].innerText && squares[8].innerText !== ''){
           console.log(' Case3 win')
           document.getElementById('line7Dec').style.display = 'block'
-          if (squares[8].innerText === 'X') {
-            props.addX()          }
-          else{props.addO()}
-          // refresh()
+          AwardWin(8,props)
         }
         else if(squares[1].innerText === squares[4].innerText && squares[4].innerText === squares[7].innerText && squares[7].innerText !== ''){
           console.log(' Case4 win')
           document.getElementById('line4').style.display = 'block'
-          if (squares[7].innerText === 'X') {
-            props.addX()          }
-          else{props.addO()}
-          // refresh()
+          AwardWin(7,props)
         }
         else if(squares[2].innerText === squares[5].innerText && squares[5].innerText === squares[8].innerText && squares[8].innerText !== ''){
           console.log(' Case5 win')
           document.getElementById('line5').style.display = 'block'
-          if (squares[8].innerText === 'X') {
-            props.addX()          }
-          else{props.addO()}
-          // refresh()
+          AwardWin(8,props)
         }
         else if(squares[3].innerText === squares[4].innerText && squares[4].innerText === squares[5].innerText && squares[5].innerText !== ''){
           console.log(' Case6 win')
           document.getElementById('line3').style.display = 'block'
-          if (squares[5].innerText === 'X') {
-            props.addX()          }
-          else{props.addO()}
-          // refresh()
+          AwardWin(5,props)
         }
         else if(squares[6].innerText === squares[7].innerText && squares[7].innerText === squares[8].innerText && squares[8].innerText !== ''){
           console.log(' Case7 win')
           document.getElementById('line6').style.display = 'block'
-          if (squares[8].innerText === 'X') {
-            props.addX()          }
-          else{props.addO()}
-          // refresh()
+          AwardWin(8,props)
         }
         else if(squares[2].innerText === squares[4].innerText && squares[4].innerText === squares[6].innerText && squares[6].innerText !== ''){
           console.log(' Case8 win')
           document.getElementById('line8Dec').style.display = 'block'
-          if (squares[6].innerText === 'X') {
-            props.addX()          }
-          else{props.addO()}
-          // refresh()
+          AwardWin(6,props)
         }
         else{console.log('No one won')
-        return true
     }
       }   
 
