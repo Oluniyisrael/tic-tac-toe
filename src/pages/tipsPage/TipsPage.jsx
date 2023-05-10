f2cf6b29ff65f9ca552c26b98501ad4f4be1a15d
Oluniyisrael <israeloluniyi2004@gmail.com>
2023-05-10 12:10:37 +0100

    Still designing, buh getting tired

--- src/pages/tipsPage/TipsPage.jsx
+++ src/pages/tipsPage/TipsPage.jsx
@@ -10,16 +10,18 @@ function TipsPage() {
             <div id='imageCont'>
                 <div className='ImgText'>
                     <img src={EmptyBoard} alt="..." className='img'/>
-                    <p>In Tic Tac Toe, You have 3 rows and 3 columns</p>
+                    <p>In Tic Tac Toe, You have 3 rows and 3 columns.</p>
                 </div>
                 <div className='ImgText'>
                     <img src={TipsGif} alt="..." className='img'/>
-                    <p>The goal of the game is to have your tally 3 in a row vertically, horizontally or diagonaly.</p>
+                    <p>The goal of the game is to have your tally 3 in a row horizontally,vertically or diagonaly.</p>
                 </div>
                 <div className='ImgText'>
                     <img src={EmptyBoard} alt="..." className='img'/>
+                    <p>It is a two-player game, so you'll need another person to take turns with. </p>
                 </div>
             </div>
+            <div><button>X</button> <button>O</button></div>
         </div>
     </div>
   )
