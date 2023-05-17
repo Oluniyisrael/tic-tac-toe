38bba7af2182e5bee07cf3c46a52a9b28558ea48
Oluniyisrael <israeloluniyi2004@gmail.com>
2023-05-17 12:27:50 +0100

    Testing tesing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/landingPage/LandingPage';
-import GamePage from './pages/gamePage/GamePage';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import LandingPage from './pages/landingPage/LandingPage';
+// import GamePage from './pages/gamePage/GamePage';
 import TipsPage from './pages/tipsPage/TipsPage';
 
 function App() {
