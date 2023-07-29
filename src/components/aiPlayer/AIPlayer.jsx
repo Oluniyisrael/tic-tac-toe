4ba72451371a8203d925131aace47d7955ae7bd4
Oluniyisrael <israeloluniyi2004@gmail.com>
2023-07-29 20:29:40 +0100

    Regoc key

--- src/components/aiPlayer/AIPlayer.jsx
+++ src/components/aiPlayer/AIPlayer.jsx
@@ -1,82 +1,32 @@
 function AIPlayer(){
-//     var amount =document.getElementsByClassName('writeHere').length
-//     for (let i = 0; i < amount; i++) {
-//         var randomSquareNo = Math.round(( amount* Math.random()))
-//         square[i].addEventListener(
-//             'click',
-//             square[randomSquareNo].click()   
-
-//         )
-//     }
-//     var square = document.getElementsByClassName('writeHere')
-//     //very buggy
-// }
-// function  test () {
-//     var square = document.getElementsByClassName('writeHere')
-//     var amount = document.getElementsByClassName('writeHere').length
-//             var randomSquareNo = Math.round(( amount* Math.random()))
-//                 square[randomSquareNo].click()     
-//                 // buggy  
-// }
-// function testing (){
-//     var square = document.querySelectorAll('section')
-//     var amount = square.length
-//             var randomSquareNo = Math.round(( amount * Math.random()))
-//                 square[randomSquareNo].click()   
-// }
-//   function test2(){
     for(i=0;i < document.getElementsByClassName('writeHere').length;i++){
         document.getElementsByClassName('writeHere')[i].addEventListener('click',test2)
     
     }
-    // function test2(){
-    //     var square = document.getElementsByClassName('writeHere')
-    //     var amount = square.length
-    //             var randomSquareNo = (Math.round(( amount* Math.random())) -1)
-    //       etTimeout(() => {
-    //             square[randomSquareNo].click()
-    //         }, 2000)
-               
-    //   } 
-    //
-    // function test2(){
-    //     var square = document.getElementsByClassName('writeHere')
-    //     var amount = square.length
-    //             var randomSquareNo = (Math.round(( amount* Math.random())) -1)
-    //       setTimeout(() => {
-    //             square[randomSquareNo].click()
-    //         }, 2000)
-               
-    //   } 
-    // for(i=0;i < document.getElementsByClassName('writeHere').length;i++){
-    //     document.getElementsByClassName('writeHere')[i].addEventListener('mousedown',test2)
-    
-    // }
-    //
-    //Might be the one :/
-    // function test2(){
-    //     var square = document.getElementsByClassName('writeHere')
-    //     var amount = (square.length)
-    //     var randomSquareNo = (Math.round( Math.random() *  (amount - 1)))
-    //       setTimeout(() => {
-    //             console.log(randomSquareNo)
-    //           console.log(amount)
-    //             square[randomSquareNo].click()
+     function test2(){
+        var square = document.getElementsByClassName('writeHere')
+        var amount = (square.length)
+        var randomSquareNo = (Math.round( Math.random() *  (amount - 2)))
+        if(randomSquareNo < 0 || randomSquareNo === -0 ){randomSquareNo = 0}
+          setTimeout(() => {
+                console.log(randomSquareNo)
+              console.log(amount)
+                square[randomSquareNo].click()
             
-    //          }, 500)
+             }, 1000)
                
-    //   } 
+      } 
+      // key
     // for( let i=0;i < document.getElementsByClassName('writeHere').length -1 ;i++){
-    //     document.getElementsByClassName('writeHere')[i].addEventListener('mousedown',test2)
+    //     document.getElementsByClassName('writeHere')[i].addEventListener('mousedown',AIPlayer)
     
-    // }
-    //
+    // }// almost correct
     //
 
-    var square = document.getElementsByClassName('writeHere')
-    var amount = square.length
-            var randomSquareNo = (Math.round(( amount* Math.random())) -1)
-            square[randomSquareNo].click()
-            // correct!!
+    // var square = document.getElementsByClassName('writeHere')
+    // var amount = square.length
+    //         var randomSquareNo = (Math.round(( amount* Math.random())) -1)
+    //         square[randomSquareNo].click()
+    //         // correct!!
   }  
   export default AIPlayer
