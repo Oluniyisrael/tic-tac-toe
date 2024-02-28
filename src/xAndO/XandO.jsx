63ef2bee5b104085558d64c1359a4354b8d1ae76
Oluniyisrael <israeloluniyi2004@gmail.com>
2024-02-28 17:49:14 +0100

    Ok lemme borrow small data

--- src/xAndO/XandO.jsx
+++ src/xAndO/XandO.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import './XandO.css'
 
 
 function XandO(props) {
@@ -11,27 +12,44 @@ function XandO(props) {
   ]  
 
  const [tableCntnt, setTableCntnt] = useState(["","","","","","","","",""])
+ const [count,setCount] = useState(0)
+ const [node , changeNode] = useState(["writeHere","writeHere","writeHere","writeHere","writeHere","writeHere","writeHere","writeHere","writeHere"])
+ function handleClick(index) {
+  // for nodes
+  const exit = "changeNode";
+  const newNode = [...node]
+  newNode[index] = exit
+  changeNode(newNode)
+  setCount(count + 1)
+  // for x and o additioning
+  const newTallyInsertion = [...tableCntnt];
+  newTallyInsertion[index] = writings[0][count]
+  setTableCntnt(newTallyInsertion)
+}
+console.log(`Count: ${count}`)
 
   return (
     <div className='table'>
         <div className='tableCont'>
         {/* <div><span id='turn' >{writings[choice][count]}</span>'s turn <button onClick={()=>{}}>refres</button></div> */}
-          <table>
-          <tr>
-            <td>{tableCntnt[0]}</td>
-            <td>{tableCntnt[1]}</td>
-            <td>{tableCntnt[2]}</td>
+          <table className='table'>
+            <tbody>
+            <tr className='tableRow'>
+            <td className='tableData'>{tableCntnt[0]}<span className={node[0]} onClick={ ()=>handleClick(0)}> </span></td>
+            <td className='tableData'>{tableCntnt[1]}<span className={node[1]} onClick={ ()=>handleClick(1)}> </span></td>
+            <td className='tableData'>{tableCntnt[2]}<span className={node[2]} onClick={ ()=>handleClick(2)}> </span></td>
             </tr>
-            <tr>
-            <td>{tableCntnt[3]}</td>
-            <td>{tableCntnt[4]}</td>
-            <td>{tableCntnt[5]}</td>
+            <tr className='tableRow'>
+            <td className='tableData'>{tableCntnt[3]}<span className={node[3]} onClick={ ()=>handleClick(3)}> </span></td>
+            <td className='tableData'>{tableCntnt[4]}<span className={node[4]} onClick={ ()=>handleClick(4)}> </span></td>
+            <td className='tableData'>{tableCntnt[5]}<span className={node[5]} onClick={ ()=>handleClick(5)}> </span></td>
             </tr>
-            <tr>
-            <td>{tableCntnt[6]}</td>
-            <td>{tableCntnt[7]}</td>
-            <td>{tableCntnt[8]}</td>
+            <tr className='tableRow'>
+            <td className='tableData'>{tableCntnt[6]}<span className={node[6]} onClick={ ()=>handleClick(6)}> </span></td>
+            <td className='tableData'>{tableCntnt[7]}<span className={node[7]} onClick={ ()=>handleClick(7)}> </span></td>
+            <td className='tableData'>{tableCntnt[8]}<span className={node[8]} onClick={ ()=>handleClick(8)}> </span></td>
             </tr>
+            </tbody>
           </table>
             {/* </div> */}
         </div>
