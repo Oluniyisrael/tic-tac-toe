3228a3ae694d187f7c5feee7cda38b050416c67b
Oluniyisrael <israeloluniyi2004@gmail.com>
2024-02-29 14:06:20 +0100

    Yup

--- src/xAndO/XandO.jsx
+++ src/xAndO/XandO.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect} from 'react'
 import { useState } from 'react'
 import './XandO.css'
 
@@ -106,6 +106,8 @@ switch (winDet.winCase){
     prevLine[winDet.winCase] = "block"
     setLine(prevLine)
     break;
+    default:
+      break;
 
 }
 addChoice()
